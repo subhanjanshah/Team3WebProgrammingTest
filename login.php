a3cb36497a198697c1048191b35d4f34ec86b8d7
Maheesha Nuwangie Sumanadasa Akmeemana Madduma Acharige <maheesha53001@gmail.com>
2026-02-19 20:49:32 +0200

    Change form action from create.php to create2.php

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ include 'header.php';
     <div class="login-box">
         <h2>Welcome Back</h2>
         
-        <form action="create.php" method="POST">
+        <form action="create2.php" method="POST">
             <div class="form-group">
                 <label for="email">Email address</label>
                 <input type="email" id="email" name="email" placeholder="Enter your email" required>
